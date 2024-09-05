568d992081c0cb265dde8d225a8510260c9e27e7
ztepler <ztepler@gmail.com>
2024-09-06 00:27:10 +0300

    fix: endpoint is global node option

--- init.sh
+++ init.sh
@@ -18,7 +18,7 @@ if [ ! -f "/home/tezos/.tezos-smart-rollup-node/is_imported" ]; then
 
     echo "Downloading and importing Etherlink snapshot..."
     curl -L $ETH_GHOSTNET_SNAPSHOT -o eth-ghostnet.archive
-    octez-smart-rollup-node snapshot import eth-ghostnet.archive --data-dir /home/tezos/.tezos-smart-rollup-node --endpoint $OCTEZ_RPC_NODE
+    octez-smart-rollup-node --endpoint $OCTEZ_RPC_NODE snapshot import eth-ghostnet.archive --data-dir /home/tezos/.tezos-smart-rollup-node
     rm eth-ghostnet.archive
 
     touch /home/tezos/.tezos-smart-rollup-node/is_imported
